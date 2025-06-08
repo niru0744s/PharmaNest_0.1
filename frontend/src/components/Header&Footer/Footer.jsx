87c54dc801e7bf86376c439b2e26a8d1241d182c
niru0744s <nirupambhattacharya100@gmail.com>
2025-06-08 16:19:21 +0530

    resF&N

--- frontend/src/components/Header&Footer/Footer.jsx
+++ frontend/src/components/Header&Footer/Footer.jsx
@@ -11,7 +11,7 @@ export default function Footer() {
     <div className='footerBody border-top mt-4'>
             <div className="container mt-5 mb-0">
             <div className="row ">
-                <div className="col-6 mt-5">
+                <div className="col-lg-6 mt-5">
                     <div className='d-flex'>
                     <MailOutlineIcon fontSize='large' style={{"fontSize":"1.8rem"}}/>
                     <p style={{marginLeft:"10px"}} className='fs-5'>Sent a Message</p>
@@ -24,12 +24,9 @@ export default function Footer() {
                           </button>
                     </form>
                 </div>
-                <div className="col-6">
-                    {/* <img src="media/images/medicine.svg" alt="medicine_svg" style={{"height":"15rem" , "width":"15rem"}} className='ms-4 mb-5 ' /> */}
-                </div>
             </div>
             <div className="row">
-                    <div className="col-4 mt-5">
+                    <div className="col-lg-4 col-md-12 mt-5">
                         <h4 className='mt-2' style={{color:"#F9FAFB"}}>PharmaNest</h4>
                         <p className='mt-4' style={{color:"#D1D5DB"}}> `Pharmanest: Your trusted online medical store 
                             for high-quality medicines, health supplies, and wellness essentials. 
@@ -44,34 +41,36 @@ export default function Footer() {
                             <div className='cont'><LinkedInIcon style={{"color":"#22C55E "}} className='fs-2 icon'/></div>
                         </div>
                     </div>
-                    <div className="col-8 mt-5 d-flex footer-links">
-                        <div className='me-5 ms-5 '>
+                    <div className="col-lg-8 col-md-6 mt-5 d-flex footer-links">
+                        <div className="row">
+                            <div className='me-5 ms-5 col '>
                             <p className='f-links'>Aboutus</p>
                             <p className='f-links'>Services</p>
                             <p className='f-links'>Blog</p>
                             <p className='f-links'>Contactus</p>
-                        </div>
-                        <div className='me-5 ms-5'>
+                            </div>
+                            <div className='me-5 ms-5 col'>
                             <p className='f-links'>Support</p>
                             <p className='f-links'>Knowledge base</p>
                             <p className='f-links'>Live Chat</p>
-                        </div>
-                        <div className='me-5 ms-5'>
+                            </div>
+                            <div className='me-5 ms-5 col'>
                             <p className='f-links'>Jobs</p>
                             <p className='f-links'>Our Team</p>
                             <p className='f-links'>Privacy Policy</p>
                             <p className='f-links'>Leadership</p>
-                        </div>
-                        <div className='ms-5'>
+                            </div>
+                            <div className='ms-5 col'>
                             <p className='f-links'>Medicines</p>
                             <p className='f-links'>Health</p>
                             <p className='f-links'>Supplies</p>
                             <p className='f-links'>Care</p>
+                            </div>
                         </div>
                     </div>
             </div>
             <hr className='mt-5'/>
-            <div className="col-6">
+            <div className="col-lg-6">
                 <p className='' style={{color:"#D1D5DB"}}>Copyright ©2025. All Rights Reserved. Design & Build by <a href="https://www.linkedin.com/in/nirupam-bhattacharya-5a36a4257/" target='_blank' className='text-danger stratched-link' style={{"textDecoration":"none",color:"#60A5FA"}}>Nirupam Bhattacharya</a></p>
             </div>
             <div className="col-6"></div>
